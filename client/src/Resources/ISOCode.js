04889272ecc393e8f7d94179cfe3fa4c852da000
Manavesh Narendra <manavesh.narendra@gmail.com>
2022-01-19 18:06:18 +0530

    Added static resources to map codes to states for API calls

---
+++ client/src/Resources/ISOCode.js
@@ -0,0 +1,10 @@
+const slugs = [
+  "bihar",
+  "west-bengal",
+  "jharkhand",
+  "odisha",
+  "andaman-and-nicobar",
+];
+const ISO = ["BR", "WB", "JH", "OR", "AN"];
+
+export { slugs, ISO };
---
+++ client/src/Resources/Zone.js
@@ -0,0 +1,10 @@
+const zones = {
+  EastZone: [
+    { name: "Bihar", slug: "bihar", code: "e1" },
+    { name: "Jharkhand", slug: "jharkhand", code: "e2" },
+    { name: "West Bengal", slug: "west-bengal", code: "e3" },
+    { name: "Odisha", slug: "odisha", code: "e4" },
+    { name: "Andaman and Nicobar", slug: "andaman-and-nicobar", code: "e5" },
+  ],
+};
+module.exports = zones;
