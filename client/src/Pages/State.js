2e93e09b921e1a5f0008d54d17a8fecde2540494
Manavesh Narendra <manavesh.narendra@gmail.com>
2022-01-20 21:05:38 +0530

    Refactored the Code to District Card by additional props

--- client/src/Pages/State.js
+++ client/src/Pages/State.js
@@ -1,30 +1,54 @@
 //importing react libraries
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 //importing components
 import DistrictCard from "../Components/DistrictsWise/DistrictCard.js";
-//importing 3rd party packages
+import StateCard from "../Components/State/StateCard.js";
 import objectToArray from "object-to-array";
+//importing 3rd party packages
+import { slugs, ISO } from "../Resources/ISOCode.js";
 import { getStateDetailAxios } from "../Components/Functions/axios.js";
 const State = () => {
+  const dispatch = useDispatch();
+  const { state } = useSelector((state) => ({ ...state }));
+  console.log("from redux : ", state);
   const params = useParams();
   console.log(params.state_code);
+
+  //ON RELOADING, THE REDUX GETS LOST, SO ENSURE WE HAVE THE CODE..
+  const indexWhenReduxEmpty = slugs.indexOf(params.state_code);
+  const code = ISO[indexWhenReduxEmpty];
+
+  if (state === null) {
+    dispatch({
+      type: "STATE",
+      payload: code,
+    });
+  }
   const [districtReport, setDistrictReport] = useState([]);
+  const [total, setTotal] = useState([]);
+  const [meta, setMeta] = useState([]);
   useEffect(() => {
     getStateDetail();
-  }, []);
+  }, [state]);
 
   const getStateDetail = () => {
-    getStateDetailAxios("BR")
+    getStateDetailAxios(state)
       .then((res) => {
-        setDistrictReport(objectToArray(res.data.JH.districts));
+        console.log(res);
+        setDistrictReport(objectToArray(res.districts));
+        setTotal(objectToArray(res.delta));
+        console.log(res.meta.last_updated);
+        setMeta(res.meta?.last_updated);
       })
       .catch((err) => console.log(err));
   };
   return (
     <>
-      <DistrictCard districtReport={districtReport} />
-      {JSON.stringify(districtReport.length)}
+      <br />
+      <StateCard total={total} meta={meta} />
+      <DistrictCard districtReport={districtReport} className="p-4 ms-4" />
     </>
   );
 };
