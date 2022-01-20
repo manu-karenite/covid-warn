d75fc802e476623275b929787b51c565f98c11bb
Manavesh Narendra <manavesh.narendra@gmail.com>
2022-01-20 20:56:21 +0530

    Added Title Component for Site

---
+++ client/src/Components/Navigation/Title.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <div
+      className="jumbotron display-6 text-center text-success mt-2 mb-2"
+      style={{ fontSize: "50px" }}
+    >
+      <b>Covid Counter</b>
+    </div>
+  );
+};
+
+export default Title;
