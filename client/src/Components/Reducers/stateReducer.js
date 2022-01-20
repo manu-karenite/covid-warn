826204c6e52dce06b26aa516cf3b302e951fca9c
Manavesh Narendra <manavesh.narendra@gmail.com>
2022-01-20 21:04:19 +0530

    Removed unwanted case from state Redux

--- client/src/Components/Reducers/stateReducer.js
+++ client/src/Components/Reducers/stateReducer.js
@@ -1,8 +1,6 @@
 const stateReducer = (state = null, action) => {
   switch (action.type) {
-    case "STATE_SELECTED":
-      return action.payload;
-    case "STATE_NOT_SELECTED":
+    case "STATE":
       return action.payload;
     default:
       return state;
