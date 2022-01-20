e39fdfe58e393a6c353124bd7967e278a2fbcba3
Manavesh Narendra <manavesh.narendra@gmail.com>
2022-01-20 21:06:11 +0530

    Resources updated for all UTs and States

--- client/src/Resources/Zone.js
+++ client/src/Resources/Zone.js
@@ -1,10 +1,75 @@
 const zones = {
   EastZone: [
+    //5
     { name: "Bihar", slug: "bihar", code: "e1" },
     { name: "Jharkhand", slug: "jharkhand", code: "e2" },
     { name: "West Bengal", slug: "west-bengal", code: "e3" },
     { name: "Odisha", slug: "odisha", code: "e4" },
     { name: "Andaman and Nicobar", slug: "andaman-and-nicobar", code: "e5" },
+  ],
+  NorthEastZone: [
+    //8
+    { name: "Sikkim", slug: "sikkim", code: "ne1" },
+    { name: "Arunachal Pradesh", slug: "arunachal-pradesh", code: "ne2" },
+    { name: "Mizoram", slug: "mizoram", code: "ne3" },
+    { name: "Meghalaya", slug: "meghalaya", code: "ne4" },
+    { name: "Assam", slug: "assam", code: "ne5" },
+    { name: "Nagaland", slug: "nagaland", code: "ne6" },
+    { name: "Tripura", slug: "tripura", code: "ne7" },
+    { name: "Manipur", slug: "manipur", code: "ne8" },
+  ],
+  SouthZone: [
+    //7
+    { name: "Andhra Pradesh", slug: "andhra-pradesh", code: "s1" },
+    { name: "Karnataka", slug: "karnataka", code: "s2" },
+    { name: "Kerala", slug: "kerala", code: "s3" },
+    { name: "Lakshadweep", slug: "lakshadweep", code: "s4" },
+    { name: "Puducherry", slug: "puducherry", code: "s5" },
+    { name: "Tamil Nadu", slug: "tamil-nadu", code: "s6" },
+    { name: "Telangana", slug: "telangana", code: "s7" },
+  ],
+  CentralZone: [
+    //4
+    { name: "Chhattisgarh", slug: "chhattisgarh", code: "c1" },
+    { name: "Madhya Pradesh", slug: "madhya-pradesh", code: "c2" },
+    { name: "Maharashtra", slug: "maharashtra", code: "c3" },
+    { name: "Uttar Pradesh", slug: "uttar-pradesh", code: "c4" },
+  ],
+  WesternZone: [
+    //3
+    { name: "Gujarat", slug: "gujarat", code: "w1" },
+    { name: "Goa", slug: "goa", code: "w2" },
+    {
+      name: "Dadra and Nagar Haveli and Daman and Diu",
+      slug: "dadra-and-nagar-haveli-and-daman-and-diu",
+      code: "w3",
+    },
+  ],
+  NorthernZone: [
+    { name: "Chandigarh", slug: "chandigarh", code: "n1" },
+    { name: "Delhi", slug: "delhi", code: "n2" },
+    {
+      name: "Haryana",
+      slug: "haryana",
+      code: "n3",
+    },
+    { name: "Himachal Pradesh", slug: "himachal-pradesh", code: "n4" },
+    { name: "Jammu And Kashmir", slug: "jammu-and-kashmir", code: "n5" },
+    {
+      name: "Ladakh",
+      slug: "ladakh",
+      code: "n6",
+    },
+    {
+      name: "Punjab",
+      slug: "punjab",
+      code: "n7",
+    },
+    {
+      name: "Uttarakhand",
+      slug: "uttarakhand",
+      code: "n8",
+    },
   ],
 };
 module.exports = zones;
