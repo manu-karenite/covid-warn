23fc4aa8ff0ead9acc3a2c1282cf6795b19ac8cc
Manavesh Narendra <manavesh.narendra@gmail.com>
2022-01-20 20:59:52 +0530

    Added Title Component in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,17 @@ import { Fragment } from "react";
 
 //importing routing in App.js
 import { Link, Routes, Route } from "react-router-dom";
-
+import { useSelector } from "react-redux";
 //importing utilities in the App.js
 import Header from "./Components/Navigation/Header.js";
 import SideMenu from "./Components/Navigation/SideMenu.js";
+import Title from "./Components/Navigation/Title.js";
 //importing pages on web App
 import Home from "./Pages/Home.js";
 import State from "./Pages/State.js";
 
 function App() {
+  const { state } = useSelector((state) => ({ ...state }));
   return (
     <Fragment>
       <Header />
@@ -21,6 +23,7 @@ function App() {
             <SideMenu />
           </div>
           <div className="col-md-10">
+            {!state && <Title />}
             <Routes>
               <Route path="/" exact element={<Home />} />
               <Route path="/state/:state_code" exact element={<State />} />
