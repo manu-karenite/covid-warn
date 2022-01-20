377cf86306cd9efae3c9036759af9fb2066bb6f5
Manavesh Narendra <manavesh.narendra@gmail.com>
2022-01-20 21:02:28 +0530

    Used Redux to dispatch state=Null when on home page

--- client/src/Components/Navigation/Header.js
+++ client/src/Components/Navigation/Header.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 //importing 3rd party components....
+import { useDispatch } from "react-redux";
 import { Menu } from "antd";
 import {
   MailOutlined,
@@ -10,40 +11,25 @@ import {
 const { SubMenu } = Menu;
 
 const Header = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const [state, setState] = useState("home");
   const handleClick = (e) => {
     console.log(e);
     setState(e.key);
   };
+  const clickHandler = (e) => {
+    //first dispatch the hook to redux.........
+    dispatch({
+      type: "STATE",
+      payload: null,
+    });
+    navigate("/");
+  };
   return (
     <Menu onClick={handleClick} selectedKeys={[state]} mode="horizontal">
-      <Link to="/">
-        <Menu.Item key="home" icon={<AppstoreOutlined />}>
-          Home
-        </Menu.Item>
-      </Link>
-
-      <Menu.Item key="app" disabled icon={<AppstoreOutlined />}>
-        Navigation Two
-      </Menu.Item>
-      <SubMenu
-        key="SubMenu"
-        icon={<SettingOutlined />}
-        title="Navigation Three - Submenu"
-      >
-        <Menu.ItemGroup title="Item 1">
-          <Menu.Item key="setting:1">Option 1</Menu.Item>
-          <Menu.Item key="setting:2">Option 2</Menu.Item>
-        </Menu.ItemGroup>
-        <Menu.ItemGroup title="Item 2">
-          <Menu.Item key="setting:3">Option 3</Menu.Item>
-          <Menu.Item key="setting:4">Option 4</Menu.Item>
-        </Menu.ItemGroup>
-      </SubMenu>
-      <Menu.Item key="alipay">
-        <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-          Navigation Four - Link
-        </a>
+      <Menu.Item key="home" icon={<AppstoreOutlined />} onClick={clickHandler}>
+        Home
       </Menu.Item>
     </Menu>
   );
