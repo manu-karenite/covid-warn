a2f845ab3bedc66a9040519a5dc4faf93e7a5ce3
Manavesh Narendra <manavesh.narendra@gmail.com>
2022-01-21 15:13:42 +0530

    Added Reducer for Searching the Districts

---
+++ client/src/Components/Reducers/searchReducer.js
@@ -0,0 +1,9 @@
+const searchReducer = (state = [], action) => {
+  switch (action.type) {
+    case "SEARCH":
+      return action.payload;
+    default:
+      return state;
+  }
+};
+export default searchReducer;
