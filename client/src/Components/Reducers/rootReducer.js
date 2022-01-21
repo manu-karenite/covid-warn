e71b7eb3ac502e4f0713d1dacc48e96c2128517c
Manavesh Narendra <manavesh.narendra@gmail.com>
2022-01-21 15:19:47 +0530

    Combined Search Reducer to Root Reducer

--- client/src/Components/Reducers/rootReducer.js
+++ client/src/Components/Reducers/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import stateReducer from "./stateReducer.js";
+import searchReducer from "./searchReducer.js";
 const rootReducer = combineReducers({
   state: stateReducer,
+  search: searchReducer,
 });
 export default rootReducer;
