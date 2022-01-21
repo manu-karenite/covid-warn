6ce63cd78fccfda9dfd51a349f7a5ad3c4b8d1b5
Manavesh Narendra <manavesh.narendra@gmail.com>
2022-01-21 15:24:20 +0530

    Final Commit

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,8 +1,50 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import {
+  getStateDetailAxios,
+  getEveryDetailAxios,
+} from "../Components/Functions/axios.js";
+import objectToArray from "object-to-array";
+import StateCard from "../Components/State/StateCard.js";
+import StateTable from "../Components/State/StateTable.js";
 const Home = () => {
   document.title = `Covid Counter`;
-  return <div>home</div>;
+  const [total, setTotal] = useState([]);
+  const [meta, setMeta] = useState([]);
+  const [stateWise, setStateWise] = useState([]);
+  useEffect(() => {
+    getCasesIndia();
+  }, []);
+  useEffect(() => {
+    getAllCasesIndia();
+  }, []);
+  const getCasesIndia = () => {
+    getStateDetailAxios("TT")
+      .then((res) => {
+        setTotal(objectToArray(res.delta));
+        setMeta(res.meta?.last_updated);
+      })
+      .catch((err) => console.log(err));
+  };
+  const getAllCasesIndia = () => {
+    getEveryDetailAxios()
+      .then((res) => {
+        setStateWise(res);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <>
+      <StateCard total={total} meta={meta} />
+
+      <hr />
+      <div className="container-fluid">
+        <div className="row">
+          <StateTable stateWise={stateWise} />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
--- client/src/Pages/State.js
+++ client/src/Pages/State.js
@@ -1,7 +1,8 @@
 //importing react libraries
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
+import { LoadingOutlined } from "@ant-design/icons";
 //importing components
 import DistrictCard from "../Components/DistrictsWise/DistrictCard.js";
 import StateCard from "../Components/State/StateCard.js";
@@ -10,11 +11,13 @@ import objectToArray from "object-to-array";
 import { slugs, ISO } from "../Resources/ISOCode.js";
 import { getStateDetailAxios } from "../Components/Functions/axios.js";
 const State = () => {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [up, setUp] = useState(true);
   const dispatch = useDispatch();
   const { state } = useSelector((state) => ({ ...state }));
-  console.log("from redux : ", state);
+
   const params = useParams();
-  console.log(params.state_code);
 
   //ON RELOADING, THE REDUX GETS LOST, SO ENSURE WE HAVE THE CODE..
   const indexWhenReduxEmpty = slugs.indexOf(params.state_code);
@@ -34,21 +37,37 @@ const State = () => {
   }, [state]);
 
   const getStateDetail = () => {
+    setLoading(true);
     getStateDetailAxios(state)
       .then((res) => {
-        console.log(res);
         setDistrictReport(objectToArray(res.districts));
         setTotal(objectToArray(res.delta));
-        console.log(res.meta.last_updated);
+
+        dispatch({
+          type: "SEARCH",
+          payload: objectToArray(res.districts),
+        });
         setMeta(res.meta?.last_updated);
+        setLoading(false);
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
   };
   return (
     <>
       <br />
+
+      {loading && <LoadingOutlined />}
       <StateCard total={total} meta={meta} />
-      <DistrictCard districtReport={districtReport} className="p-4 ms-4" />
+
+      <DistrictCard
+        districtReport={districtReport}
+        setUp={setUp}
+        up={up}
+        className="p-4 ms-4"
+      />
     </>
   );
 };
