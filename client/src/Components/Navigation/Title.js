68f766b5c1a46c451f86d5483992d47bca712327
Manavesh Narendra <manavesh.narendra@gmail.com>
2022-01-21 15:23:59 +0530

    Typography Changed for Title

--- client/src/Components/Navigation/Title.js
+++ client/src/Components/Navigation/Title.js
@@ -3,8 +3,8 @@ import React from "react";
 const Title = () => {
   return (
     <div
-      className="jumbotron display-6 text-center text-success mt-2 mb-2"
-      style={{ fontSize: "50px" }}
+      className="jumbotron display-6 text-center text-success mt-2 mb-3 "
+      style={{ fontSize: "50px", fontWeight: "bold", letterSpacing: "2.5px" }}
     >
       <b>Covid Counter</b>
     </div>
